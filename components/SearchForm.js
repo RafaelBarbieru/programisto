8c218526b16ae15607751bd3cd1bc0da5d173dd7
Rafael Barbieru <rafael@MBPtilhdeRafael.lan>
2022-02-17 15:22:25 +0100

    Restructures the layout and css

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -4,19 +4,21 @@ import styles from "../styles/SearchForm.module.css";
 export default function SearchForm() {
   return (
     <form className={styles.container}>
-      <div className={ styles.title }>
-        <h1>Programisto</h1>
-      </div>
       <div className={styles.form}>
-        <label id="lb_keywords" className={styles.label}>
-          Keywords (separated with commas)
-        </label>
-        <input id="in_keywords" type="text" className={styles.text_input} />
-        <label id="lb_location" className={styles.label}>
-          Location (country or city)
-        </label>
-        <input id="in_location" type="text" className={styles.text_input} />
-        <input type="submit" value="Search" className={styles.button} />
+        <div className={styles.title}>
+          <h1>Programisto</h1>
+        </div>
+        <div className={styles.inner_form}>
+          <label id="lb_keywords" className={styles.label}>
+            Keywords (separated with commas)
+          </label>
+          <input id="in_keywords" type="text" className={styles.text_input} />
+          <label id="lb_location" className={styles.label}>
+            Location (country or city)
+          </label>
+          <input id="in_location" type="text" className={styles.text_input} />
+          <input type="submit" value="Search" className={styles.button} />
+        </div>
       </div>
     </form>
   );
